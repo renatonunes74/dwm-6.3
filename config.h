75b522318096cdaedae6021c5a1d0515b5d88b5b
renatonunes74 <renato.s.nunes74@gmail.com>
2022-10-24 12:09:08 -0300

    add dwm-taglabels+hide_vacant_tags & dwm-pertag

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Cozette:size=10" };
 static const char dmenufont[]       = "Cozette:size=10";
@@ -16,11 +16,15 @@ static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray1, col_cyan  },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const char ptagf[] = "%s %s"; /* format of a tag label */
+static const char etagf[] = "%s";    /* format of an empty tag */
+static const int lcaselbl = 0; /* 1 means make tag label lowercase */
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -63,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-c", "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -74,11 +78,11 @@ static Key keys[] = {
 	{ 0,                       	XF86XK_AudioLowerVolume,      spawn,          SHCMD("amixer -c 0 set Master 5db-") },
 	{ 0,                       	XF86XK_AudioRaiseVolume,      spawn,          SHCMD("amixer -c 0 set Master 5db+") },
 	{ 0,                       	XF86XK_AudioMute,		spawn,          SHCMD("amixer -c 0 set Master toggle") },
-	{ MODKEY,                      	XK_w,      spawn,          SHCMD("st w3m duckduckgo.com") },
+	{ MODKEY,                      	XK_w,      spawn,          SHCMD("st -c w3m w3m duckduckgo.com") },
 	{ MODKEY,                      	XK_q,      spawn,          SHCMD("qutebrowser") },
 	{ MODKEY,                      	XK_y,      spawn,          SHCMD("qutebrowser https://y.com.sb/feed/popular") },
-	{ MODKEY,                      	XK_e,      spawn,          SHCMD("st sfeed_update $HOME/.sfeed/sfeedrc && sfeed_curses $HOME/.sfeed/feeds/*") },
-	{ MODKEY|ShiftMask,            	XK_m,      spawn,          SHCMD("st mutt") },
+	{ MODKEY,                      	XK_e,      spawn,          SHCMD("st -c sfeed sfeed_update $HOME/.sfeed/sfeedrc && sfeed_curses $HOME/.sfeed/feeds/*") },
+	{ MODKEY|ShiftMask,            	XK_m,      spawn,          SHCMD("st -c mutt mutt") },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
